cfdbf296279bb1564ceb3f31855083706585bc18
Zach Shallbetter <zachshallbetter@gmail.com>
2014-05-09 11:36:36 -0700

    removed outdated screenshots

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-/**
- * Created with JetBrains PhpStorm.
- * User: buihoangvu
- * Date: 10/4/13
- * Time: 3:28 PM
- * To change this template use File | Settings | File Templates.
- */
-
 window.dd = function(msg)
 {
   console.log(msg);
